9a41db6f8179608b28e9a9a352d1571d76bfd766
Valentina Avalos <sammwy.dev@gmail.com>
2021-07-10 17:54:50 -0300

    Uploaded initial source

---
+++ src/CacheManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace NCache {
+    public class CacheManager {
+        
+        private Dictionary<string, CachePool> pools;
+        private List<string> cacheWhitelist;
+        
+        public CacheManager () {
+            this.pools = new Dictionary<string, CachePool>();
+            this.cacheWhitelist = new List<string>();
+
+            this.cacheWhitelist.Add("text/css");
+            this.cacheWhitelist.Add("text/js");
+            this.cacheWhitelist.Add("application/javascript");
+            this.cacheWhitelist.Add("image/png");
+            this.cacheWhitelist.Add("image/jpg");
+            this.cacheWhitelist.Add("image/jpeg");
+            this.cacheWhitelist.Add("image/svg+xml");
+        }
+
+        public void CreatePool (string hostname) {
+            this.pools.Add(hostname, new CachePool());
+        }
+
+        public CachePool GetPool (string hostname) {
+            if (this.pools.ContainsKey(hostname))
+                return this.pools[hostname];
+            else
+                return null;
+        }
+
+        public void AddResource (string hostname, string path, CachedResource resource) {
+            if (this.GetPool(hostname) == null) {
+                this.CreatePool(hostname);
+            }
+
+            this.GetPool(hostname).Add(path, resource);
+        }
+
+        public CachedResource GetResource (string hostname, string path) {
+            if (this.GetPool(hostname) != null) {
+                return this.GetPool(hostname).Get(path);
+            }
+
+            return null;
+        }
+
+        public bool MustBeCached (string contentType) {
+            contentType = contentType.Split(";")[0];
+            return cacheWhitelist.Contains(contentType);
+        }
+    }
+}
---
+++ src/CachedResource.cs
@@ -0,0 +1,23 @@
+namespace NCache {
+    public class CachedResource {
+        private string contentType;
+        private byte[] buffer;
+
+        public CachedResource (string contentType, byte[] buffer) {
+            this.contentType = contentType;
+            this.buffer = buffer;
+        }
+
+        public byte[] Buffer {
+            get {
+                return this.buffer;
+            }
+        }
+
+        public string ContentType {
+            get {
+                return this.contentType;
+            }
+        }
+    }
+}
